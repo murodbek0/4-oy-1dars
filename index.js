6256edf3d8b7283dc06b190acf755c5842d464ac
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-07-15 02:32:23 +0500

    matter

---
+++ index.js
@@ -0,0 +1,122 @@
+// 1 misol
+// 2 misol
+// 3 misol
+
+
+// 4 misol
+// let num1 = 2;
+// let num2 = 5;
+// let num3 = 10;
+// let gotdressed = num1 * num1 + num2 * num2 + num3 * num3;
+// console.log(gotdressed);
+
+// 5 misol
+// let a = 2;
+// let b = 5;
+// let degree = a ** b;
+// console.log(degree);
+
+// 6 misol
+// 7 misol
+
+
+// 8 misol
+// let num1 = 15;
+// let result = num1 *10;
+// let sedated = result -7;
+// console.log(result);
+// console.log(sedated);
+
+
+// 9 misol
+// let num = 27;
+// let degree = Math.cbrt(num);
+// console.log(degree);
+
+// 10 misol
+// let num1 = 2;
+// let num2 = 3;
+// let num3 = 4;
+
+// let averageNumber = Math.cbrt((num1 * num2 * num3));
+// console.log(averageNumber);
+
+
+// 11 misol
+// let sm = +prompt('uzunlikni krting');
+// let result = Math.trunc(sm / 100);
+// console.log(result);
+
+// 12 misol
+// let m = +prompt('ogrlikni krting');
+// let result = Math.trunc(m / 1000);
+// console.log(result);
+
+// 13 misol
+// let bayt = +prompt('fayil xachmini krting');
+// let result = Math.trunc(bayt / 1024);
+// console.log(result);
+
+// 14 misol
+// let a = +prompt('a ');
+// let b = +prompt('b');
+// let result = Math.trunc ( a / b);
+// console.log(result);
+
+// 15 misol
+// let a = +prompt('a');
+// let b = +prompt('b');
+// let result = Math.trunc(a%b);
+// console.log(result);
+
+// 16 misol
+// alert('2 honali son kriting');
+// let A = +prompt("A");
+// let H = Math.floor(A/10);
+// console.log(H);
+// let v = Math.floor(A%10);
+// console.log(v);
+
+
+// 17 misol
+// alert('2 honali son kriting');
+// let A = +prompt("A");
+// let H = Math.floor(A/10);
+// console.log(H);
+// let v = Math.floor(A%10);
+// console.log(v);
+// let P = Math.floor(v+H);
+// console.log(P);
+
+// 18 misol
+
+// alert('2 honali son kriting');
+// let B = +prompt("B");
+// let N = Math.floor(B/10);
+// let C = Math.floor(B%10);
+// let D = Math.floor((C*10)+N);
+// console.log(D);
+
+// 19 misol
+//  alert('3 honali son krting');
+//  let s = +prompt('s');
+//  let m = Math.floor(s/100);
+//  console.log (m);
+
+// 20 misol
+    //  alert('3 honali son krting');
+// let B = +prompt("B");
+// let a = Math.floor(B%10);
+// let z = Math.floor(B/10%10);
+// console.log(a);
+// console.log(z);
+
+// 21 misol
+// 22 misol
+// 23 misol
+// alert('3 honali son berrilgan')
+// let B = +prompt("B");
+// let b = Math.floor(B/100);
+// let o = Math.floor(B/10%10);
+// let y = Math.floor(B%10);
+// console.log((o*100)+(y*10)+b);
